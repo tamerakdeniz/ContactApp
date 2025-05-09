53cf366135c3b54a0be4569779d3b2919b421d9f
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2025-05-09 09:02:06 +0300

    Design feature additions

---
+++ app/src/main/java/com/example/contactapp/ui/adapter/ContactAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.contactapp.ui.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.contactapp.data.entity.Kisiler
+import com.example.contactapp.databinding.CardDesignBinding
+import com.example.contactapp.ui.fragment.HomePageFragmentDirections
+import com.google.android.material.snackbar.Snackbar
+
+
+class ContactAdapter(var mContext: Context, var kisilerListesi:List<Kisiler>)
+    : RecyclerView.Adapter<ContactAdapter.CardTasarimTutucu>() {
+
+    inner class CardTasarimTutucu(var tasarim: CardDesignBinding) : RecyclerView.ViewHolder(tasarim.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): CardTasarimTutucu {
+        val tasarim = CardDesignBinding.inflate(LayoutInflater.from(mContext), parent, false)
+        return CardTasarimTutucu(tasarim)
+
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        val kisi = kisilerListesi.get(position) // 0,1,2
+        val t = holder.tasarim
+        t.textViewAd.text = kisi.kisi_ad
+        t.textViewTel.text = kisi.kisi_tel
+
+        t.cardViewLine.setOnClickListener {
+            val gecis = HomePageFragmentDirections.contactDetailTransition(kisi = kisi)
+            Navigation.findNavController(it).navigate(gecis)
+        }
+
+        t.imageViewDelete.setOnClickListener {
+            Snackbar.make(it,"${kisi.kisi_ad} should it be deleted?", Snackbar.LENGTH_SHORT)
+                .setAction("Yes") {
+                    sil(kisi.kisi_id)
+                }.show()
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return kisilerListesi.size
+
+    }
+
+    fun sil(kisi_id:Int) {
+        Log.e("Kişi Sil",kisi_id.toString())
+    }
+}
--- app/src/main/java/com/example/contactapp/ui/fragment/HomePageFragment.kt
+++ app/src/main/java/com/example/contactapp/ui/fragment/HomePageFragment.kt
@@ -1,14 +1,19 @@
 package com.example.contactapp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.contactapp.R
 import com.example.contactapp.data.entity.Kisiler
 import com.example.contactapp.databinding.FragmentHomePageBinding
+import com.example.contactapp.ui.adapter.ContactAdapter
 
 class HomePageFragment : Fragment() {
     private lateinit var binding: FragmentHomePageBinding
@@ -22,13 +27,42 @@ class HomePageFragment : Fragment() {
             Navigation.findNavController(it).navigate(R.id.addContactTransition)
         }
 
-        binding.buttonDetail.setOnClickListener {
-            val x = Kisiler(1,"Tamer","1111")
-            val gecis = HomePageFragmentDirections.contactDetailTransition(kisi=x)
-            Navigation.findNavController(it).navigate(gecis)
-        }
+        val kisilerListesi = ArrayList<Kisiler>()
+        val k1 = Kisiler(1,"Mustafa","1111")
+        val k2 = Kisiler(2,"Tamer","2222")
+        val k3 = Kisiler(3,"Akdeniz","3333")
+        kisilerListesi.add(k1)
+        kisilerListesi.add(k2)
+        kisilerListesi.add(k3)
 
+        val kisilerAdapter = ContactAdapter(requireContext(),kisilerListesi)
+        binding.kisilerRv.adapter = kisilerAdapter
+
+        binding.kisilerRv.layoutManager = LinearLayoutManager(requireContext()) // Alt Alta Sıralar
+
+        // binding.kisilerRv.layoutManager = StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL) - aynı düzeyde sağa doğru kaydırmalı
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String): Boolean { //Harf girdikçe ve sildikçe çalışacak
+                ara(newText)
+                return true
+
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean { //Arama butonuna basınca çalışacak
+                ara(query)
+                return true
+            }
+        })
 
         return binding.root
+    }
+    fun ara(aramaKelimesi:String) {
+        Log.e("Search Contact",aramaKelimesi)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("Contact Home Page","returned")
     }
 }
